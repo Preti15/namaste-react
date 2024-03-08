13363db887ac0859b885ccc901e9def5a8ae833b
Preti Yadav <pretiyadav@pretis-MacBook-Pro.local>
2024-03-08 12:48:21 +0530

    chore: Episode 3 - foundation

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-const heading = React.createElement("h1", {id:"heading"}, "hello there from React");
+import  React  from 'react';
+import  ReactDOM  from 'react-dom/client';
+
+
+const Heading = () => ( React.createElement("h1", {id:"heading"}, "hello there from React"));
+
+//jsx is converted by transpilier or compiler which is Babel into create react element (object) and then by render into HTML elements.
+const JsxHeading = () => {
+return(
+<div>
+    <Heading />
+    <h1 className='heading' >this is hello from Functional Component</h1>
+</div>
+)
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(<JsxHeading />);
